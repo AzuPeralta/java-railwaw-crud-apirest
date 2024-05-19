d462f5029fba7eea2063b564d5d4dee8a6d6b474
Peralta, Mariela <marielajperalta@gmail.com>
2024-05-19 17:44:45 -0300

    config cors

--- java-crud-apirest-master/src/main/java/com/azuperalta/apirest/apirest/config/WebConfig.java
+++ java-crud-apirest-master/src/main/java/com/azuperalta/apirest/apirest/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://listado-de-productos-react.netlify.app"," https://listadoproductos-react.netlify.app", "http://localhost:5173/") 
+                .allowedOrigins("https://listadoproductos-react.netlify.app", "http://localhost:5173/") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
