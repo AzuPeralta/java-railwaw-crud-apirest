bc2815ca2a6359fb9274e80957ee691172b77469
Peralta, Mariela <marielajperalta@gmail.com>
2024-06-07 01:17:35 -0300

    Se agrega url a permisos de CORS

--- java-crud-apirest-master/src/main/java/com/azuperalta/apirest/apirest/config/WebConfig.java
+++ java-crud-apirest-master/src/main/java/com/azuperalta/apirest/apirest/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://listadoproductos-react.netlify.app", "http://localhost:5173/") 
+                .allowedOrigins("https://listadoproductos-react.netlify.app","https://master--listadoproductos-react.netlify.app/", "http://localhost:5173/") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
